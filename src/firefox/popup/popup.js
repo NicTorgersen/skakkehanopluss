8f610fcf36d0fbb3f38d2c68d58cbc9c6b6cef22
Nichlas Torgersen <nichlas.torgersen@gmail.com>
2017-12-07 21:14:54 +0100

    begynte arbeid med å kunne velge aviser å blokkere

--- src/firefox/popup/popup.js
+++ src/firefox/popup/popup.js
@@ -2,43 +2,57 @@
 
     function saveVerboseOption (state) {
         browser.storage.sync.set({'verbose': state}, function () {
-            notifyChange("Lagret endringer.")
+            notifyState()
         })
     }
 
-    function notifyChange (message) {
+    function notifyState (message, style) {
+        message = message || "Lagret endringer."
+        style = style || false
+
+        if (style) {
+            $("#notify-change").toggleClass("."+style);
+        }
+
         $("#notify-change").empty().text(message)
-        $("#notify-change").fadeIn(400, () => {})
+        $("#notify-change").fadeIn(400)
+
         setTimeout(() => {
-            $("#notify-change").fadeOut(400, () => {})
+            $("#notify-change").fadeOut(400)
         }, 1000)
     }
 
     function toggleOption (key) {
         var optionPromise = browser.storage.sync.get(key)
 
-        optionPromise.then(function (optionValue) { // success
-            optionValue = optionValue || ''
-            if (optionValue.length != 0) {
+        optionPromise.then(function (optionValue) {
+            optionValue = optionValue || false
 
+            if (optionValue) {
+                browser.storage.sync.set({key: !optionValue})
+                    .then(null, onError)
+            } else {
+                browser.storage.sync.set({key: false})
+                    .then(null, onError)
             }
-        }, function () { // error
-
-        })
+        }, onError)
     }
 
-    browser.storage.onChanged.addListener((changes, namespaces) => {
-        for (var key in changes) {
-            var storageChange = changes[key]
-            for (var input in inputs) {
-                var el = inputs[input]
-                if (key == input) {
-                    el.prop('checked', storageChange)
-                }
+    function onError (err) {
+        notifyState("err: "+err, "error")
+    }
+
+    // derp...
+    function getHostFromURL (url) {
+        var protocols = ["http://", "https://"]
+        for (var i = 0; i < protocols.length; i++) {
+            var protocol = protocols[i]
+            if (url.startsWith(protocol)) {
+                url = url.replace(protocol, "").replace("/", "")
             }
         }
-    });
-
+        return url
+    }
 
     $(document).ready(function () {
         var verboseCheckbox = $("#verbose")
@@ -48,15 +62,40 @@
             'unblock': "Jæ har pluss på denna sia.",
             'block': "Jæ vi'kke ha no' pluss her lenger!",
         }
+
+        browser.storage.onChanged.addListener((changes, namespaces) => {
+            for (key in changes) {
+                if (key === "verbose") {
+                    verboseCheckbox.prop('checked', changes[key])
+                } else {
+                    browser.tabs.query({
+                        active: true,
+                        currentWindow: true,
+                    }, function (tabs) {
+                        var tab = tabs[0]
+                        var host = getHostFromURL(tab.url)
+                        console.log(host)
+                    })
+                }
+            }
+        });
         
         storagePromise.then(function (storageItems) => {
-            for (itemKey in storageItems) {
+            for (itemKey in storageItems) { // dette kommer sikkert til å bli tregt
                 if (itemKey === 'verbose') {
                     verboseCheckbox.prop('checked', storageItems['verbose'])
-                }
+                } else {
+                    browser.tabs.query({ // spesielt pga dette
+                        active: true,
+                        currentWindow: true,
+                    }, function (tabs) {
+                        var tab = tabs[0]
+                        var host = getHostFromURL(tab.url)
 
-                if (itemKey === window.location.host) {
-                    
+                        if (itemKey === host) {
+
+                        }
+                    })
                 }
             }
         })
